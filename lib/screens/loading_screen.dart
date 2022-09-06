5a315ca928562e47e1c18c5e677bd11f5481f2db
JaeHyuk Choi <jaehyuk1220@gmail.com>
2022-09-06 15:14:19 +0900

    Location 정보 받아와서 출력하도록 구현 - Android Manifast, iOS .plist 파일 수정하여 위치정보 권한설정 - iOS cocoapod 버전이 안맞아서 빌드가 안되서어서 cocoa pod 버전을 -v 1.11.0 로 재설치 -  LocationPermission permission = await Geolocator.requestPermission(); 없이는 권한설정 팝업이 뜨지 않아 추가수정

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -6,6 +7,17 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  void getLocation() async {
+    print(1);
+
+    LocationPermission permission = await Geolocator.requestPermission();
+    print(2);
+
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.low);
+    print(position);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,6 +25,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
         child: RaisedButton(
           onPressed: () {
             //Get the current location
+            getLocation();
           },
           child: Text('Get Location'),
         ),
